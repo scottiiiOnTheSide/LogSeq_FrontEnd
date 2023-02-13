7ea64843c01dada6e8fe1bd36785c5687baa4033
scottiiiOnTheSide <roviajce@gmail.com>
2023-02-13 01:46:50 -0500

    See notes.txt

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import MenuButton from './components/menuButton/menuButton';
 function App() {
 
   /*Self explanatory*/
-  const apiAddr = 'http://172.27.7.39:3333';
+  const apiAddr = 'http://172.23.61.18:3333';
 
   /*User Log In*/
   //07. 07. 2022 These two should honestly be one in the same. Will couple them later
--- src/components/userMenu/userMenu.jsx
+++ src/components/userMenu/userMenu.jsx
@@ -2,29 +2,32 @@
 import React, {useState, useReducer, useEffect} from 'react';
 import './userMenu.css';
 
-const formReducer = (state, event) => {
-	return {
-		...state,
-		[event.name]: event.value
-	}
-}
 
 function CreatePost({apiAddr, userKey, updateLog, calendar}) {
 
-	const [formData, setFormData] = useReducer(formReducer, {});
+	const [formData, setFormData] = useState({});
+	const [content, setContent] = useState([
+		{
+			content: null,
+			index: null
+		}
+	]);
+	let contents = [];
 
-	const handleChange = (event) => {
+	const handleChange = (event, index) => {
 
 		if(event.target.name == 'tags') {
 			let value = event.target.value;
 			let array = value.split(/[, ]+/);
 			setFormData({
+				...formData,
 				name: event.target.name,
 				value: array
 			})
 		}
 		else {
 			setFormData({
+				...formData,
 				name: event.target.name,
 				value: event.target.value
 			})
@@ -78,21 +81,47 @@ function CreatePost({apiAddr, userKey, updateLog, calendar}) {
 		updateLog();
 	}
 
+	const newCombo = (e) => {
+		e.preventDefault();
+
+		setContent([
+			...content,
+			{
+				content: null,
+				index: null,
+			}
+		])
+		console.log(content);
+	}
+
+	let textareaImageAdd = () => {
+		let element = <fieldset className="textareaImageadd">
+			<textarea 
+				name="content" 
+				placeholder="Content" 
+				onChange={handleChange}
+				rows="10"
+				cols="30">
+			</textarea>
+			<input id="addImage" type="file" name='image'/>
+			<img /> 
+		</fieldset>
+
+		return element;
+	}
 	return (
 		//form with title, content tags
 		<div id="createPost">
 			<form onSubmit={handleSubmit}>
 				<fieldset>
 					<input name="title" placeholder="Title" onChange={handleChange}/>
-					<textarea 
-						name="content" 
-						placeholder="Content" 
-						onChange={handleChange}
-						rows="10"
-						cols="30"></textarea>
+					{content.map((element, index) => (
+						textareaImageAdd()
+					))}
 					<input name="tags" placeholder="tags" onChange={handleChange}/>
 				</fieldset>
 				<button type="submit">Submit</button>
+				<button onClick={newCombo}>New TextArea + ImageAdd</button>
 			</form>
 
 			{/*<button onClick={toggleCreateForm}>Close</button>*/}
